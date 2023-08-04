903fcec840df9002545b4a2018270686f9e2586d
brandoncoding18 <b7ggm8@gmail.com>
2023-08-04 04:30:58 -0400

    8/4/2023 - attack functionality finished

--- projects/art-tussle.py
+++ projects/art-tussle.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import requests
 
 # GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
 intents = discord.Intents.default()
@@ -8,7 +9,62 @@ intents.message_content = True
 bot = discord.Client(intents=intents)
 
 members = []
+pending_attacks = []
+pending_images = []
+pending_charid = []
+pending_quality = []
+pending_auth = [] 
 permitted_channel_id = 1136358266868346950
+
+
+class Attack: 
+
+    def __init__(self, attacker, recipient, url, charid, quality, auth):
+        self.attacker = attacker
+        self.recipient = recipient
+        self.url = url
+        self.charid = charid
+        self.quality = quality 
+        self.auth = auth
+
+    def getAttacker(self): 
+        return self.attacker
+    
+    def getRecipient(self): 
+        return self.recipient
+    
+    def getUrl(self):
+        return self.url
+
+    def getCharID(self):
+        return self.charid
+
+
+    def getQuality(self):
+        return self.quality
+
+    def getAuth(self): 
+        return self.auth
+    #
+
+    def setRecipient(self, recipient):
+        self.recipient = recipient
+    
+    def setUrl(self, url):
+        self.url = url
+
+    def setCharID(self, charid):
+        self.charid = charid
+
+    def setQuality(self, quality):
+        self.quality = quality
+
+    def setAuth(self, auth): 
+        self.auth = auth 
+
+    def __str__ (self):
+        return "Attacker:" + str(self.attacker) + "\nRecipient:" + str(self.recipient) +  "\nUrl:" + str(self.url) +  "\nCharID:"+ str(self.charid) +  "\nQuality:" + str(self.quality) +  "\nAuth:" + str(self.auth) +  "\n"
+    
 
 # EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
 # Method calls on startup
@@ -21,6 +77,9 @@ async def on_ready():
             members.append(str(member.id))
     print(members)
 
+async def send_dm(ctx, member: discord.Member, *, content):
+    channel = await member.create_dm()
+    await channel.send(content)
 
 @bot.event
 async def on_member_join(member):
@@ -30,8 +89,11 @@ async def on_member_join(member):
 
 @bot.event
 async def on_member_remove(member):
-    members.append(str(member.id))
+    members.remove(str(member.id))
 
+async def sendDm(id, content):
+    user = await bot.fetch_user(id)
+    await user.send(str(content))
 
 
 @bot.event  
@@ -44,64 +106,184 @@ async def on_reaction_add(reaction, user):
 @bot.event
 async def on_message(message):
     channel = bot.get_channel(int(permitted_channel_id)) 
-    if(message.channel.id == 1136358266868346950 and not str(message.author) == "Arttussle#3770"):
-        msg = str(message.content)
-        msg = msg.split(" ")
-        print(msg)
-        if(msg[0] == "/attack"):
-            recipient_id = msg[1].replace("<", '').replace(">", '').replace("@", '')
-            if(str(recipient_id) == str(message.author.id)): 
-                await message.channel.send("Cannot attack user with same id")
+    print(message.channel.type)
+
+    if(not str(message.author) == "Art Arena#4949"):
+        if(str(message.channel.type) == "private"):
+            local_url = ""
+            recipient =  None
+            img = (message.attachments)
+            index = 0
+            for i in range(0, len(pending_attacks)):
+                    if(pending_attacks[i].getAttacker().id == message.author.id):
+                        recipient = pending_attacks[i].getRecipient()
+                        index = i
+                        print("hiiiii")
+
+            print(pending_attacks[index])
+            if(message.author in pending_images and len(img) == 1):
                 
-            elif(recipient_id in members):
-                await message.channel.send("Attacking " + msg[1])
-                await message.channel.send("Please check your direct messages for instructions on your submission: " + "<@" + str(message.author.id) + ">")
-                hash = 12345
-                thread = await channel.create_thread(name="Attack " + str(hash), type=discord.ChannelType.private_thread)
-                await thread.send("Attack thread with id: " + str(hash) + 
-                                "\nAttacker: <@" + str(message.author.id) + ">" +  
-                                "\nRecipient: " + msg[1] +
-                                "\nCharacter: [id: char_name]" +
-                                "\nQuality: [quality]" + 
-                                "\nCalculated Score: [score]")
-                await thread.send(file=discord.File('sample_attack.jpg'))
-                await thread.send("Attack log will be updated shortly")
+                
+                img = str(img)
+                await message.channel.send(str(message.attachments))
+                url = img[img.index("url"): len(img)].replace("url=", "").replace(">]", "").replace("\'", "")
+                await message.channel.send(url)
+                print(url)
+                ftype = url.split('/')[-1]
+                myfile = requests.get(url)
+                local_url = f'/tmp/file.{ftype}'
+                open(local_url, 'wb').write(myfile.content)
+                pending_images.remove(pending_attacks[index].getAttacker())
+                pending_attacks[index].setUrl(local_url)
+                await message.channel.send(str(pending_attacks[index]))
+                await message.channel.send("Please submit character id")
+
+            elif(message.author in pending_charid):
+                pending_attacks[index].setCharID(message.content)
+                await message.channel.send(str(pending_attacks[index]))
+
+                pending_charid.remove(pending_attacks[index].getAttacker())
+                await message.channel.send("Please submit quality:\n1: Rough sketch\n2: Finished Lineart\n3:Rough sketch with color\n4: Clean Color no shading\n5: Clean Color Minimal Shading\n6:Clean Color Complex Shading")
 
 
-            else: 
-                await message.channel.send("User not found")
-            
-          
+
+
+            elif(message.author in pending_quality):
+                if(message.content in ["1", "2", "3", "4", "5", "6"]):
+                    pending_attacks[index].setQuality(message.content)
+                    await message.channel.send(str(pending_attacks[index]))
+                    pending_quality.remove(pending_attacks[index].getAttacker())
+                    await message.channel.send("Please confirm your attack:(Y/N)")
+
+            elif(message.author in pending_auth):
+                if(message.content.lower() == "y"):
+                    await message.channel.send("Submitting Attack")
+                    pending_auth.remove(pending_attacks[index].getAttacker())
+                    pending_attacks[index].setAuth("approved")
+
+                    channel = await bot.fetch_channel(permitted_channel_id)
+                    #await channel.send(file=discord.File(local_url))
+                    thread = await channel.create_thread(name="Attack:" , type=discord.ChannelType.private_thread)
+                    await thread.send("Attack thread with id: " + str(hash(recipient)) + 
+                                    "\nAttacker: <@" + str(message.author.id) + ">" +  
+                                    "\nRecipient: <@" + str(pending_attacks[index].getRecipient()) + ">" +
+                                    "\nCharacter: " + str(pending_attacks[index].getCharID()) +
+                                    "\nQuality: " + str(pending_attacks[index].getQuality()) +
+                                    "\nCalculated Score: " + str(pending_attacks[index].getQuality()))
+                    await thread.send(file=discord.File(str(pending_attacks[index].getUrl())))
+                    await thread.send("Attack log will be updated shortly")
+
+
+
+
                     
+                elif(message.content.lower() == "n"):
+                    await message.channel.send("Please resubmit the image for your attack")
+                    pending_images.append(pending_attacks[index].getAttacker())
+                    pending_charid.append(pending_attacks[index].getAttacker())
+                    pending_quality.append(pending_attacks[index].getAttacker())
+                else: 
+                    await message.channel.send("Please respond with either Y or N")
 
-                
-            #except: 
-                await message.channel.send("no arguments given \n\n Commands: \n\n /attack [user] [] ")
-        
-        if(msg[0] == "/fight-history"):
-            try: 
-                target = msg[1].replace("<", '').replace(">", '').replace("@", '')
-                if(target in members):
-                    await message.channel.send("[Unimplemented Feature]")
+            
+
+                #this is where database insert happens 
+
+            #await message.channel.send(str(message.attachments))
+       
+
+
+
+
+
+
+
+
+
+
+        if(message.channel.id == permitted_channel_id):
+            msg = str(message.content)
+            msg = msg.split(" ")
+            print(msg)
+
+            if(str(msg[0]) + " " + str(msg[1])  == "kill yourself"):
+                await message.channel.send("Disconnecting")
+                await bot.close()
+            if(msg[0] == "/attack"):
+                recipient_id = msg[1].replace("<", '').replace(">", '').replace("@", '')
+
+                #IF THE ID OF THE ATTACKER IS THE SAME AS THE RECIPIENT, REJECT 
+                #if(str(recipient_id) == str(message.author.id)): 
+                    #await message.channel.send("Cannot attack user with same id")
+                    
+                #IF THE ID OF THE ATTACKER IS IN THE LIST OF MEMBERS
+                if(recipient_id in members):
+                    await message.channel.send("Attacking " + msg[1])
+                    await message.channel.send("Please check your direct messages for instructions on your submission: " + "<@" + str(message.author.id) + ">")
+                    recipient = await bot.fetch_user(str(recipient_id))
+                    recipient = recipient.id
+                    attacker = message.author
+                    print("hiiiiiiiiii")
+                    pending_attacks.append(Attack(attacker, recipient, "", "", "", ""))
+                    pending_images.append(attacker)
+                    pending_charid.append(attacker)
+                    pending_quality.append(attacker)
+                    pending_auth.append(attacker)
+                    await sendDm(message.author.id,  "Please submit the image you would like to submit as an attack")
+
+
+
+
+
+
+
+
+                    #hash = 12345
+                    #thread = await channel.create_thread(name="Attack " + str(hash), type=discord.ChannelType.private_thread)
+                    #await thread.send("Attack thread with id: " + str(hash) + 
+                                    # "\nAttacker: <@" + str(message.author.id) + ">" +  
+                                    # "\nRecipient: " + msg[1] +
+                                    #  "\nCharacter: [id: char_name]" +
+                                    #   "\nQuality: [quality]" + 
+                                    #   "\nCalculated Score: [score]")
+                    # await thread.send(file=discord.File('sample_attack.jpg'))
+                    #await thread.send("Attack log will be updated shortly")
+                    #this is where database insert happens 
+
 
                 else: 
                     await message.channel.send("User not found")
-
-
-
-
-            except:
-                await message.channel.send("User not provided, please provide argument like so: /fight-history @[user]")
-
-        if(msg[0] == "/leaderboard"):
-            
-            try: 
-                if(msg[1]):
-                    await message.channel.send("/leaderboard does not accept additional arguments")
                     
+                
+                            
 
-            except:
-                await message.channel.send("[Unimplemented]")
+                        
+                #await message.channel.send("no arguments given \n\n Commands: \n\n /attack [user] [] ")
+            
+            if(msg[0] == "/fight-history"):
+                try: 
+                    target = msg[1].replace("<", '').replace(">", '').replace("@", '')
+                    if(target in members):
+                        await message.channel.send("[Unimplemented Feature]")
+
+                    else: 
+                        await message.channel.send("User not found")
+
+
+
+
+                except:
+                    await message.channel.send("User not provided, please provide argument like so: /fight-history @[user]")
+
+            if(msg[0] == "/leaderboard"):
+                
+                try: 
+                    if(msg[1]):
+                        await message.channel.send("/leaderboard does not accept additional arguments")
+                        
+
+                except:
+                    await message.channel.send("[Unimplemented]")
 
 
         
@@ -117,4 +299,4 @@ async def on_message(message):
 
     
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
-bot.run("MTEzNjM1NzU1NjIyNzQwODA3Mg.GmrYwg.Jx74TZKNAqSnxVnZPIBPLDacBZRVrxGRDWYRps")
+bot.run("MTEzNjkxMDEzODIxNzg3NzUwNg.GZJKol.eDuO3ftLNHYy7SIPpsA2AMbrRGG4-w9XX9bqRA")
